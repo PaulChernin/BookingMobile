4e8d9f368b5b08ddf22406d242bfaffac9b73415
PaulChernin <pavel@chernin.ru>
2022-06-10 14:27:52 +0300

    removed put request

--- app/src/main/java/com/paulchernin/booking/data/RoomRequest.kt
+++ app/src/main/java/com/paulchernin/booking/data/RoomRequest.kt
@@ -15,8 +15,6 @@ class RoomRequest(
     fun get(filter: RoomFilter, callback: (Array<Room>) -> Unit) {
         val db = DbAccess()
 
-        Log.d("mylog", pathGet(filter))
-
         db.getArray(pathGet(filter), context) {
             val roomsList = parseJson(it)
 
@@ -27,22 +25,11 @@ class RoomRequest(
     fun get(deviceId: String, callback: (Array<Room>) -> Unit) {
         val db = DbAccess()
 
-        Log.d("mylog", deviceId)
-
         db.getArray(pathGet(deviceId), context) {
             val roomsList = parseJson(it)
 
             callback(roomsList)
         }
-    }
-
-    fun put(room: Room, callback: () -> Unit) {
-        val db = DbAccess()
-        db.putObject(pathPut(room), makeJson(room), context, callback)
-    }
-
-    private fun pathPut(room: Room): String {
-        return "/rooms/${room.id}"
     }
 
     private fun pathGet(filter: RoomFilter): String {
@@ -56,12 +43,6 @@ class RoomRequest(
         return "/bookedRooms?deviceId=$deviceId"
     }
 
-    private fun makeJson(room: Room): JSONObject {
-        val gson = Gson()
-
-        return JSONObject(gson.toJson(room))
-    }
-
     private fun parseJson(jsonArray: JSONArray): Array<Room> {
         val gson = GsonBuilder().setDateFormat("yyyy-MM-dd").create()
 
@@ -70,22 +51,5 @@ class RoomRequest(
             gson.fromJson(jsonObject.toString(), Room::class.java)
         }
 
-//        try {
-//            return Array(jsonArray.length()) {
-//                val jsonObject = jsonArray.get(it) as JSONObject
-//                Room(
-//                    jsonObject.getInt("id"),
-//                    jsonObject.getString("name"),
-//                    jsonObject.getInt("price"),
-//                    jsonObject.getInt("rooms"),
-//                    jsonObject.getBoolean("withBreakfast"),
-//                    jsonObject.getString("picture"),
-//                    jsonObject.optInt("bookingId")
-//                )
-//            }
-//        } catch (e: Exception) {
-//            Log.e("mylog", "Can't parse json")
-//            return emptyArray()
-//        }
     }
 }
